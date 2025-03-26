91ea798c56359e69064425ef638ac162213672b5
mayurk224 <mayurkamble0250@gmail.com>
2025-03-26 16:04:01 +0530

    added profile and logout route also installed cookieparser dependency

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,7 @@
 const { validationResult } = require("express-validator");
 const userService = require("../services/user.service");
 const userModel = require("../models/user.model");
+const blacklistTokenModel = require("../models/blacklistToken.model");
 
 module.exports.registerUser = async (req, res, next) => {
   try {
@@ -53,5 +54,19 @@ module.exports.loginUser = async (req, res, next) => {
 
   const token = user.generateAuthToken();
 
+  res.cookie("token", token);
+
   res.status(200).json({ user, token });
+};
+
+module.exports.getProfile = async (req, res, next) => {
+  const user = req.user;
+  res.status(200).json({ user });
+};
+
+module.exports.logoutUser = async (req, res, next) => {
+  res.clearCookie("token");
+  const token = req.cookies.token || req.headers.authorization?.split(' ')[1]
+  await blacklistTokenModel.create({token})
+  res.status(200).json({ message: "User logged out" });
 };
--- models/user.model.js
+++ models/user.model.js
@@ -37,9 +37,7 @@ const userSchema = new mongoose.Schema({
 });
 
 userSchema.methods.generateAuthToken = function () {
-  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
-    expiresIn: "7d",
-  });
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
   return token;
 };
 
